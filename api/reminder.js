97d6e751bf27ea7306878b231a3470b05848ed26
wolfiee42 <saifalislam2022@gmail.com>
2025-09-18 00:14:57 +0600

    fix: added type config in package.json and error handling in reminder func

--- api/reminder.js
+++ api/reminder.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
-
 export default async function handler(req, res) {
   try {
-    await axios.post(WEBHOOK_URL, {
+    const url = process.env.DISCORD_WEBHOOK_URL;
+    if (!url) {
+      return res
+        .status(500)
+        .json({ success: false, error: "Webhook URL not set" });
+    }
+    await axios.post(url, {
       content: "⏰ Time to stretch and move around! 💪🧘",
     });
+
     res.status(200).json({ success: true, message: "Reminder sent!" });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
